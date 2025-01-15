ded898872ffd71cd7301e85ca31e87870f3094b0
Michelle Sweeney <msween@byu.edu>
2025-01-14 17:26:35 -0700

    finished adding all the addpieces() methods

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -13,7 +13,14 @@ public class ChessBoard {
     public ChessBoard() {
         board = new ChessPiece[9][9];
     }
-
+    public void PrintChessBoard() {
+        for (int i = 7; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                System.out.print(board[i][j].pieceType + " " + board[i][j].teamColor + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public void addPiece(ChessPosition position, ChessPiece piece) {
         this.board[position.getRow()][position.getColumn()] = piece;
@@ -38,76 +45,47 @@ public class ChessBoard {
             //System.out.println(this.board[row][i].teamColor + "1" + i);
         }
     }
-    public void addRooks(){
+    public void AddRooks(){
         //adds white rooks
-        addPiece(new ChessPosition(1,1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1,1),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
         addPiece(new ChessPosition(1,8),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
         //adds black rooks
         addPiece(new ChessPosition(8,1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
         addPiece(new ChessPosition(8,8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
     }
-    /*
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
-     */
-    //resets White's side of the board
-    /*public void resetWhite(){
-        ChessGame.TeamColor teamColor = new ChessGame.TeamColor.WHITE;
-        for(ChessPiece.PieceType pieceType : ChessPiece.PieceType.values()) {
-            ChessPiece piece = new ChessPiece(teamColor, pieceType);
-            switch (pieceType) {
-                //ask the TAs
+    public void AddKnights(){
+        addPiece(new ChessPosition(1,2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1,7),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        //adds black knights
+        addPiece(new ChessPosition(8,2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8,7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+    }
+    public void AddBishops(){
+        addPiece(new ChessPosition(1,3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1,6),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        //adds bishops
+        addPiece(new ChessPosition(8,3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8,6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+    }
+    public void AddKings(){
+        addPiece(new ChessPosition(1,4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8,4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+    }
+    public void AddQueens(){
+        addPiece(new ChessPosition(1,5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8,5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
 
-                    break;
-                case KNIGHT:
-                    //addPiece(new ChessPosition(1,1), piece);
-                    //addPiece(new ChessPosition(1, 6), piece);
-                case ROOK:
-                    //addPiece(new ChessPosition(1,1), piece);
-                    //addPiece(new ChessPosition(1,8), piece);
-                case BISHOP:
-                    //addPiece(new ChessPosition(1, 2), piece);
-                    //addPiece(new ChessPosition(1, 5), piece);
-                case KING:
-                    //addPiece(new ChessPosition(0, 3), piece);
-                case QUEEN:
-                    addPiece(new ChessPosition(0, 4), piece);
-            }
-        }
     }
-    */
-    //resets black's side of the board
-    /*public void resetBlack(ChessGame.TeamColor teamColor){
-        for(ChessPiece.PieceType pieceType : ChessPiece.PieceType.values()) {
-            ChessPiece piece = new ChessPiece(teamColor, pieceType);
-            //just updated so that piece changes
-            switch (piece.pieceType) {
-                case PAWN:
-                    for (int i = 0; i < 8; i++) {
-                        addPiece(new ChessPosition(6, i), piece);
-                    }
-                case ROOK:
-                    addPiece(new ChessPosition(7,0), piece);
-                    addPiece(new ChessPosition(7,7), piece);
-                case KNIGHT:
-                    addPiece(new ChessPosition(7,1), piece);
-                    addPiece(new ChessPosition(7, 6), piece);
-                case BISHOP:
-                    addPiece(new ChessPosition(7, 2), piece);
-                    addPiece(new ChessPosition(7, 5), piece);
-                case KING:
-                    addPiece(new ChessPosition(7, 3), piece);
-                case QUEEN:
-                    addPiece(new ChessPosition(7, 4), piece);
-            }
-        }
-    }
-    */
     public void resetBoard() {
         AddPawns(ChessGame.TeamColor.WHITE);
         AddPawns(ChessGame.TeamColor.BLACK);
-        //resets both sides of the board
-        //resetWhite(ChessGame.TeamColor.WHITE);
-        //resetBlack(ChessGame.TeamColor.BLACK);
+        AddRooks();
+        AddBishops();
+        AddKnights();
+        AddQueens();
+        AddKings();
+
+        PrintChessBoard();
+
     }
 }
