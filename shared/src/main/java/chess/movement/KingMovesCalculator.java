1839f7b569d8767b442bb73ec77bb6eb6393d6d6
Michelle Sweeney <msween@byu.edu>
2025-01-21 09:49:17 -0700

    started KingMovesCalculator.java and I figured out the logic for calculating valid moves!!! Finally!!

--- shared/src/main/java/chess/movement/KingMovesCalculator.java
+++ shared/src/main/java/chess/movement/KingMovesCalculator.java
@@ -12,14 +12,14 @@ public class KingMovesCalculator implements PieceMovesCalculator{
     IsInsideBoard insideBoard = new IsInsideBoard();
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition chessPosition, ChessPiece piece) {
-        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+        Collection<ChessMove> moves = new ArrayList<>();
         //THIS IS ALL FOR A WHITE KING CHESSPIECE
 
         ChessPosition forward = new ChessPosition(chessPosition.getRow() + 1, chessPosition.getColumn());
         //if moving forward is inside the board, and the next space is empty, or its an enemy piece, its a valid move
         if(insideBoard.insideBoard(forward) && ((board.getPiece(forward) == null) || ( board.getPiece(forward) != null && board.getPiece(forward).getTeamColor() != piece.getTeamColor()))) {
             moves.add(new ChessMove(chessPosition, forward, null));
-        };
+        }
 
         ChessPosition backward = new ChessPosition(chessPosition.getRow() - 1, chessPosition.getColumn());
         //if moving backward is inside the board, and the next space is empty, or its an enemy piece, its a valid move
@@ -37,5 +37,6 @@ public class KingMovesCalculator implements PieceMovesCalculator{
         //or diagonally right
         //unless a friendly piece is in the way
         //and unless it goes outside the board
+    return moves;
     }
 }
