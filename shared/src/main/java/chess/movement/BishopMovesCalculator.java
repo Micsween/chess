ab6de579567657efb47126db175293a485a32481
Michelle Sweeney <msween@byu.edu>
2025-01-23 12:59:23 -0700

    finished BishopMovesCalculator()

--- shared/src/main/java/chess/movement/BishopMovesCalculator.java
+++ shared/src/main/java/chess/movement/BishopMovesCalculator.java
@@ -5,12 +5,42 @@ import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMovesCalculator implements PieceMovesCalculator{
-
+    IsInsideBoard isInsideBoard = new IsInsideBoard();
+    //can move diagonal right up {row + 1, col +1}
+    //can move diagonal left up {row + 1, col - 1}
+    //can move diagonal right down {row - 1, col + 1}
+    //can move diagonal left down {row -1, col - 1}
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition chessPosition, ChessPiece piece) {
-        return null;
+        Collection<ChessMove> moves = moveDiagonal(board, chessPosition, piece , 1, 1);
+        moves.addAll(moveDiagonal(board, chessPosition, piece , 1, -1));
+        moves.addAll(moveDiagonal(board, chessPosition, piece , -1, 1));
+        moves.addAll(moveDiagonal(board, chessPosition, piece , -1, -1));
+        return moves;
+    }
+    public Collection<ChessMove> moveDiagonal(ChessBoard board, ChessPosition chessPosition, ChessPiece piece, int rowModifier, int colModifier) {
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+        //create a new end position, check if its valid, and then add it to the collection of moves
+        int row = chessPosition.getRow() + rowModifier;
+        int col= chessPosition.getColumn() + colModifier;
+        ChessPosition endPosition = new ChessPosition(row, col);
+        while(isInsideBoard.insideBoard(endPosition)) {
+            if(board.getPiece(endPosition) !=null) {
+                if(board.getPiece(endPosition).getTeamColor() != piece.getTeamColor()) {
+                    moves.add(new ChessMove(chessPosition, endPosition, null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(chessPosition, endPosition, null));
+
+            row+=rowModifier;
+            col+=colModifier;
+            endPosition = new ChessPosition(row, col);
+        }
+        return moves;
     }
 }
--- shared/src/main/java/chess/movement/QueenMovesCalculator.java
+++ shared/src/main/java/chess/movement/QueenMovesCalculator.java
@@ -7,7 +7,7 @@ import chess.ChessPosition;
 import java.util.Collection;
 
 public class QueenMovesCalculator implements PieceMovesCalculator{
-
+    //queen can move any number of spaces forward, backwards, or diagonally
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition chessPosition, ChessPiece piece) {
         return null;
