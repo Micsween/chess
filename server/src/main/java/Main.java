a1616457a25dd4d7ade13ed09576f81b30b1cf67
Michelle Sweeney <msween@byu.edu>
2025-02-18 10:14:29 -0700

    locally hosting server on port 8080 using main works

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,14 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        try{
+            server.run(8080);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
