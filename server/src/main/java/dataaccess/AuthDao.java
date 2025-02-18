d78fa6d003ec94a337c65e4144935fc043fa0dce
Michelle Sweeney <msween@byu.edu>
2025-02-18 10:56:26 -0700

    Finished GameDAO Interface

--- server/src/main/java/dataaccess/AuthDao.java
+++ server/src/main/java/dataaccess/AuthDao.java
@@ -6,17 +6,22 @@ public interface AuthDao {
     /**
      * creates a new authorization given AuthData
      */
-    public void createAuth(AuthData authData); //authData service will PROVIDE authToken
+    void createAuth(AuthData authData); //authData service will PROVIDE authToken
     /**
      * returns AuthData if there exists and authorization for the given authToken, null if otherwise
      */
-    public AuthData getAuth(String authToken); //returns AuthData; returns null if there is non matching the given authtoken
+    AuthData getAuth(String authToken); //returns AuthData; returns null if there is non matching the given authtoken
     /**
      * returns true if there exists and authorization for the given authToken
      */
-    public boolean verifiedAuth(String authToken);
+    boolean verifiedAuth(String authToken);
     /**
      * deletes an authorization given an authToken
      */
-    public void deleteAuth(String authToken);
+    void deleteAuth(String authToken);
+
+    /**
+     * Deletes ALL current AuthData
+     */
+    void clearAllAuth();
 }
